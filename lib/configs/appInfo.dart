90502f5fd823c000f9798d6207e6c6be121a2f24
Fschmatz <fschmatz@hotmail.com>
2021-05-03 12:59:30 -0300

    Version 3.3.9

--- lib/configs/appInfo.dart
+++ lib/configs/appInfo.dart
@@ -20,104 +20,104 @@ class AppInfo extends StatelessWidget {
           title: Text("App Info"),
           elevation: 0,
         ),
-        body: SingleChildScrollView(
-          child: Column(children: <Widget>[
-            const SizedBox(height: 20),
-            CircleAvatar(
-              radius: 55,
-              backgroundColor: Colors.green,
-              child: CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage('assets/avatar.jpg'),
-              ),
+        body: ListView(children: <Widget>[
+          const SizedBox(height: 20),
+          CircleAvatar(
+            radius: 55,
+            backgroundColor: Colors.green,
+            child: CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('assets/avatar.jpg'),
             ),
-            const SizedBox(height: 15),
-            Text(NameChangelog.appName,
+          ),
+          const SizedBox(height: 15),
+          Center(
+            child: Text(NameChangelog.appName +" "+ NameChangelog.appVersion,
                 style: TextStyle(
                     fontSize: 17,
                     fontWeight: FontWeight.w700,
                     color: Theme.of(context).accentColor)),
-            const SizedBox(height: 15),
-            const Divider(),
-            ListTile(
-              leading: SizedBox(
-                height: 0.1,
-              ),
-              title: Text("Dev".toUpperCase(),
-                  style: TextStyle(
-                      fontSize: 13,
-                      fontWeight: FontWeight.w700,
-                      color: Theme.of(context).accentColor)),
+          ),
+          const SizedBox(height: 15),
+          const Divider(),
+          ListTile(
+            leading: SizedBox(
+              height: 0.1,
             ),
-            ListTile(
-              leading: Icon( Icons.info_outline),
-              title: Text(
-                "HAMMERED AND REDONE: 0 Times !!!",
+            title: Text("Dev".toUpperCase(),
                 style: TextStyle(
-                  fontSize: 16,
-                ),
+                    fontSize: 13,
+                    fontWeight: FontWeight.w700,
+                    color: Theme.of(context).accentColor)),
+          ),
+          ListTile(
+            leading: Icon( Icons.info_outline),
+            title: Text(
+              "HAMMERED AND REDONE: 0 Times !!!",
+              style: TextStyle(
+                fontSize: 16,
               ),
             ),
-            ListTile(
-              leading: SizedBox(
-                height: 0.1,
+          ),
+          ListTile(
+            leading: SizedBox(
+              height: 0.1,
+            ),
+            title: Text(
+              "( This is The Way! )",
+              style: TextStyle(
+                fontSize: 16,
               ),
-              title: Text(
-                "( This is The Way! )",
+            ),
+          ),
+          ListTile(
+            leading: SizedBox(
+              height: 0.1,
+            ),
+            title: Text(
+              "Application created using Flutter and the Dart language, used for testing and learning.",
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ),
+          const Divider(),
+          ListTile(
+            leading: SizedBox(
+              height: 0.1,
+            ),
+            title: Text("Source Code".toUpperCase(),
                 style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-            ),
-            ListTile(
-              leading: SizedBox(
-                height: 0.1,
-              ),
-              title: Text(
-                "Application created using Flutter and the Dart language, used for testing and learning.",
+                    fontSize: 13,
+                    fontWeight: FontWeight.w700,
+                    color: Theme.of(context).accentColor)),
+          ),
+          ListTile(
+            onTap: () {_launchGithub();},
+            leading: Icon(Icons.open_in_new_outlined),
+            title: Text("View on GitHub",
                 style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
+                    decoration: TextDecoration.underline,
+                    color: Colors.blue)),
+          ),
+          const Divider(),
+          ListTile(
+            leading: SizedBox(
+              height: 0.1,
             ),
-            const Divider(),
-            ListTile(
-              leading: SizedBox(
-                height: 0.1,
-              ),
-              title: Text("Github".toUpperCase(),
-                  style: TextStyle(
-                      fontSize: 13,
-                      fontWeight: FontWeight.w700,
-                      color: Theme.of(context).accentColor)),
+            title: Text("Quote".toUpperCase(),
+                style: TextStyle(
+                    fontSize: 13,
+                    fontWeight: FontWeight.w700,
+                    color: Theme.of(context).accentColor)),
+          ),
+          ListTile(
+            leading: Icon(Icons.messenger_outline),
+            title: Text(
+              "Software Engineering is a learning process, working code a side effect.",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
             ),
-            ListTile(
-              onTap: () {_launchGithub();},
-              leading: Icon(Icons.open_in_new_outlined),
-              title: Text("https://github.com/Fschmatz/hackernewsfschmtz",
-                  style: TextStyle(
-                      decoration: TextDecoration.underline,
-                      color: Colors.blue)),
-            ),
-            const Divider(),
-            ListTile(
-              leading: SizedBox(
-                height: 0.1,
-              ),
-              title: Text("Quote".toUpperCase(),
-                  style: TextStyle(
-                      fontSize: 13,
-                      fontWeight: FontWeight.w700,
-                      color: Theme.of(context).accentColor)),
-            ),
-            ListTile(
-              leading: Icon(Icons.messenger_outline),
-              title: Text(
-                "Software Engineering is a learning process, working code a side effect.",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
-              ),
-            ),
-          ]),
-        ));
+          ),
+        ]));
   }
 }
