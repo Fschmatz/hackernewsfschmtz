0dd46555e1943de0b5e08e248e5d14daf198f6c8
Fschmatz <fschmatz@hotmail.com>
2021-07-25 23:28:02 -0300

    Version 3.6.0

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:hackernewsfschmtz/classes/articlePages.dart';
-import 'package:hackernewsfschmtz/classes/story.dart';
-import 'package:hackernewsfschmtz/configs/settingsPage.dart';
-
 import 'package:hackernewsfschmtz/pages/articleList.dart';
 
 class Home extends StatefulWidget {
@@ -17,8 +14,6 @@ class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin {
   String articleType;
   String pageName;
   int indexAtual = 0;
-
-  //final GlobalKey<Home> _key = GlobalKey();
 
   //always start with TopStories
   int _currentIndex = 0;
@@ -52,85 +47,6 @@ class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin {
   void initState() {
     super.initState();
   }
-
-/*  void openBottomSheet() {
-    showModalBottomSheet(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-              topLeft: const Radius.circular(20.0),
-              topRight: const Radius.circular(20.0)),
-        ),
-        isScrollControlled: true,
-        context: context,
-        builder: (BuildContext bc) {
-          return Wrap(children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 12, 0, 12),
-              child: Column(
-                children: [
-                  Center(
-                    child: ListTile(
-                      title: Text(
-                        "Hacker News", //
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                      subtitle: Text(
-                        "news.ycombinator.com",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 14, color: Theme.of(context).hintColor),
-                      ),
-                    ),
-                  ),
-                  const Divider(),
-                  ListView.separated(
-                    separatorBuilder: (BuildContext context, int index) =>
-                        const Divider(),
-                    physics: NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    itemCount: listArticlePages.length,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        onTap: () {
-                          Navigator.of(context).pop();
-                          setState(() {
-                            _currentIndex = index;
-                          });
-                          indexAtual = index;
-                        },
-                        leading: Icon(
-                          Icons.article_outlined,
-                          color: index == _currentIndex
-                              ? Theme.of(context).accentColor.withOpacity(0.9)
-                              : Theme.of(context).hintColor,
-                        ),
-                        title: Text(
-                          listArticlePages[index].name,
-                          style: TextStyle(
-                              color: index == _currentIndex
-                                  ? Theme.of(context)
-                                      .accentColor
-                                      .withOpacity(0.9)
-                                  : Theme.of(context).textTheme.headline6.color,
-                              fontSize: 16),
-                        ),
-                        trailing: Visibility(
-                          visible: index != _currentIndex,
-                          child: Icon(Icons.keyboard_arrow_right,
-                              color: Theme.of(context).hintColor),
-                        ),
-                      );
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ]);
-        });
-  }*/
 
   @override
   Widget build(BuildContext context) {
